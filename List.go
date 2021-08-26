fbb275155e1ec7a2dcd3427a27ea204c927182f4
GOM <gom@theba.se>
2021-08-26 23:22:31 +0200

    add generic list interface

---
+++ List.go
@@ -0,0 +1,11 @@
+package ds
+
+type List interface {
+	Size() uint
+	Clear()
+	ForEach(func(interface{}))
+	Filter(func(interface{}) bool) List
+	Insert(v interface{})
+	Add(v interface{})
+	Get(index int) interface{}
+}
